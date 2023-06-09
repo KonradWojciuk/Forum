1dba92a10790c6e8dec7af911e69e77316e43370
konradwojciuk <konrad.wojciuk@microsoft.wsei.edu.pl>
2023-06-09 16:18:31 +0200

    Add ForumStatistic content

--- Forum/ForumStatistic.cs
+++ Forum/ForumStatistic.cs
@@ -9,22 +9,59 @@ namespace Forum
     public class ForumStatistic
     {
         private Forum Forum;
-        private int TotalQuestions;
-        private int TotalAnswer;
-        private int TotalUnansweredQuestions;
-        private int TotalQuestionsWithAnswers;
+        private int _totalQuestions;
+        private int _totalAnswer;
+        private int _totalUnansweredQuestions;
+        private int _totalQuestionsWithAnswers;
 
         public ForumStatistic(Forum forum)
         {
             Forum = forum;
-            TotalQuestions = 0;
-            TotalAnswer = 0;
-            TotalUnansweredQuestions = 0;
-            TotalQuestionsWithAnswers = 0;
+            _totalQuestions = 0;
+            _totalAnswer = 0;
+            _totalUnansweredQuestions = 0;
+            _totalQuestionsWithAnswers = 0;
 
-            
+            Forum.QuestionAdded += Forum_QuestionAdded;
+            Forum.AnswerAdded += Forum_AnswerAdded;
+        }   
+
+        public int TotalQuestions => _totalQuestions;
+        public int TotalAnswer => _totalAnswer;
+        public int TotalUnansweredQuestions => _totalUnansweredQuestions;
+        public int TotalQuestionsWithAnswer => _totalQuestionsWithAnswers;
+        public double AverageAnswersPerQuestion => _totalQuestions > 0 ? (double)TotalAnswer / TotalQuestions : 0; 
+
+        private void Forum_AnswerAdded(object sender, AnswerEventArgs e)
+        {
+            _totalAnswer++;
+            _totalUnansweredQuestions--;
+
+            if (_totalUnansweredQuestions < 0)
+                _totalUnansweredQuestions = 0;
+
+            if (!HaveAnswers(e.Answer.QuestionId))
+                _totalQuestionsWithAnswers++;
         }
 
-       
+        private void Forum_QuestionAdded(object sender, QuestionEventArgs e)
+        {
+            _totalQuestions++;
+            _totalUnansweredQuestions++;
+        }
+
+        private bool HaveAnswers(string questionId) => Forum.GetAnswerByQuestion(questionId).Count > 0;
+
+        public void DisplayStatistic()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Forum statistic:");
+            Console.WriteLine($"Number of questions: {TotalQuestions}");
+            Console.WriteLine($"Number of answers: {TotalAnswer}");
+            Console.WriteLine($"Average answers per question: {AverageAnswersPerQuestion}");
+            Console.WriteLine($"Number of question without answers: {TotalUnansweredQuestions}");
+            Console.WriteLine($"Questions with minimum one answer: {TotalQuestionsWithAnswer}");
+            Console.WriteLine();
+        }
     }
 }
